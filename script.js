c4292c6e8060f1e651a3d2b1a07213f6388c4b8b
Alyssa Schmalenberger <aschmalenberger@gmail.com>
2020-05-20 21:01:36 -0500

    finally got the functions to work and pasword to generate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var generateBtn = document.querySelector("#generate");
-
+var randomSet = [];
 function writePassword() {
 
  //find out how many charcters the user wants in their password
@@ -11,48 +11,56 @@ function writePassword() {
   var confirmUpper = confirm("would you like your password to include uppercase letters?");
   if (confirmUpper){
     var upperSet = ["A", "B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"];
+    randomSet.push(upperSet)
   }
   
   var confirmLower = confirm("would you like your password to include lowercase letters?");
   if(confirmLower){
     var lowerSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    randomSet.push(lowerSet)
   }
   
   var confirmNumber = confirm("would you like your password to include numbers?");
   if(confirmNumber){
     var numberSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    randomSet.push(numberSet)
   }
   
   var confirmSpecial = confirm("would you like your password to include special characters?");
   if(confirmSpecial){
     var specialSet = [" ","!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-",".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~" ];
-  }    
+    randomSet.push(specialSet)
+  } 
+  console.log(randomSet);   
   //use password length to determin how many characters in password- must be integer value
-  var passwordLength = parseInt(charNum);
+  
 
 // compile all the strings into one set 
 //covert sets into strings so they can be used to make the password
-  var randomSet = [upperSet.toString|| lowerSet.toString, numberSet.toString, specialSet.toString];
-  console.log(randomSet);
   
+  var passwordLength = parseInt(charNum);
   //use random formula to get a random selection from previous set// 
-  var randomChar = randomSet[Math.floor(Math.random()* randomSet.length)];
-  console.log(randomChar);
   
+  var charPassword=[];
   //set password length
   for (var i = 0; i < passwordLength; i ++){
-    return writePassword
+    var randomArray = randomSet[Math.floor(Math.random()* randomSet.length)];
+    var randomChar = randomArray[Math.floor(Math.random()* randomArray.length)];
+    console.log(randomChar);
+    
+    charPassword.push(randomChar);
+    // return writePassword
   }
+    var newPassword=charPassword.join('');
 
   //write the password... i know that something isnt working in the last few steps
   //my syntax or formulas are off but its getting close to due time so i want to have something turnedin
-  writePassword();
-  console.log(writePassword);
+
 
   //this points to the right place but isnt coming up, mostly because the above is buggy
-writePassword = document.querySelector("#password");
-console.log(characterGen * 1);
-console.log(writePassword);
+document.querySelector("#password").textContent =newPassword;
+// console.log(characterGen * 1);
+// console.log(writePassword);
 }
  
 
